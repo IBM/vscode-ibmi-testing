dd519ed47ff0b329a0762e655a016fe5db886ba6
Sanjula Ganepola <Sanjula.Ganepola@ibm.com>
2025-03-18 21:41:39 -0400

    Add line and proc coverage support
    Signed-off-by: Sanjula Ganepola <Sanjula.Ganepola@ibm.com>

--- src/fileCoverage.ts
+++ src/fileCoverage.ts
@@ -1,35 +1,28 @@
 import { DeclarationCoverage, FileCoverage, Position, StatementCoverage, TestCoverageCount, Uri } from "vscode";
+import { CoverageData } from "./types";
 
 export class IBMiFileCoverage extends FileCoverage {
-    readonly lines: StatementCoverage[] = [];
-    readonly procedures: DeclarationCoverage[] = [];
-    constructor(uri: Uri, isStatementCoverage: boolean, codeCoverage: {
-        basename: string;
-        path: any;
-        localPath: string;
-        coverage: {
-            signitures: any;
-            lineString: any;
-            activeLines: any;
-            percentRan: string;
-        };
-    }[]) {
+    public isStatementCoverage: boolean;
+    public readonly lines: StatementCoverage[] = [];
+    public readonly procedures: DeclarationCoverage[] = [];
+    constructor(uri: Uri, coverageData: CoverageData, isStatementCoverage: boolean) {
         super(uri, new TestCoverageCount(0, 0));
-        for (const file of codeCoverage) {
-            for (const [line, executed] of Object.entries(file.coverage.activeLines)) {
-                if (isStatementCoverage) {
-                    this.lines.push(new StatementCoverage(Boolean(executed), new Position(Number(line) - 1, 0)));
-                    this.statementCoverage.covered += executed ? 1 : 0;
-                    this.statementCoverage.total++;
-                } else {
-                    if (!this.declarationCoverage) {
-                        this.declarationCoverage = new TestCoverageCount(0, 0);
-                    }
+        this.isStatementCoverage = isStatementCoverage;
 
-                    this.procedures.push(new DeclarationCoverage(line, Boolean(executed), new Position(Number(line) - 1, 1)));
-                    this.declarationCoverage.covered += executed ? 1 : 0;
-                    this.declarationCoverage.total++;
+        for (const [line, executed] of Object.entries(coverageData.coverage.activeLines)) {
+            if (isStatementCoverage) {
+                this.lines.push(new StatementCoverage(Boolean(executed), new Position(Number(line) - 1, 0)));
+                this.statementCoverage.covered += executed ? 1 : 0;
+                this.statementCoverage.total++;
+            } else {
+                if (!this.declarationCoverage) {
+                    this.declarationCoverage = new TestCoverageCount(0, 0);
                 }
+
+                // TODO: What to set for declaration coverage name?
+                this.procedures.push(new DeclarationCoverage(line, Boolean(executed), new Position(Number(line) - 1, 0)));
+                this.declarationCoverage.covered += executed ? 1 : 0;
+                this.declarationCoverage.total++;
             }
         }
     }
