5077de263c5745f5ab8cc4ddcf0b264c9fbafaa7
Sanjula Ganepola <Sanjula.Ganepola@ibm.com>
2025-05-03 15:04:36 -0400

    Support remote testing config
    Signed-off-by: Sanjula Ganepola <Sanjula.Ganepola@ibm.com>

--- src/config.ts
+++ src/config.ts
@@ -3,6 +3,7 @@ import { TestingConfig } from "./types";
 import * as path from "path";
 import lodash from "lodash";
 import { Logger } from "./logger";
+import { getInstance } from "./api/ibmi";
 
 export namespace ConfigHandler {
     const TESTING_CONFIG_FILE = 'testing.json';
@@ -16,28 +17,42 @@ export namespace ConfigHandler {
 
         try {
             const localConfigUri = await findTestingConfig(workspaceFolder, uri);
-            const localConfig = localConfigUri ? await readLocalTestingConfig(localConfigUri) : undefined;
+            const localConfig = localConfigUri ? await readTestingConfig(localConfigUri, 'local') : undefined;
             if (localConfigUri && localConfig) {
                 Logger.log(LogLevel.Info, `Found local testing configuration at ${localConfigUri.toString()}:\n${JSON.stringify(localConfig, null, 2)}`);
-            } else {
-                Logger.log(LogLevel.Info, `No local testing configuration found`);
             }
 
             const globalConfigUri = Uri.joinPath(workspaceFolder.uri, GLOBAL_CONFIG_DIRECTORY, TESTING_CONFIG_FILE);
-            const globalConfig = await readLocalTestingConfig(globalConfigUri);
-            if (globalConfigUri && globalConfig) {
+            const globalConfig = await readTestingConfig(globalConfigUri, 'global');
+            if (globalConfig) {
                 Logger.log(LogLevel.Info, `Found global testing configuration at ${globalConfigUri.toString()}:\n${JSON.stringify(globalConfig, null, 2)}`);
-            } else {
-                Logger.log(LogLevel.Info, `No global testing configuration found`);
             }
 
             const mergedConfig = lodash.merge({}, globalConfig, localConfig);
-            Logger.log(LogLevel.Info, `Merged testing configuration:\n${JSON.stringify(mergedConfig, null, 2)}`);
+            Logger.log(LogLevel.Info, `Merged local testing configuration:\n${JSON.stringify(mergedConfig, null, 2)}`);
             return mergedConfig;
         } catch (error: any) {
-            Logger.logWithNotification(LogLevel.Error, `Failed to retrieve testing configuration`, error);
+            Logger.logWithNotification(LogLevel.Error, `Failed to retrieve local testing configuration`, error);
             return;
         }
+    }
+
+    export async function getRemoteConfig(uri: Uri): Promise<TestingConfig | undefined> {
+        const ibmi = getInstance();
+        const connection = ibmi!.getConnection();
+
+        const parsedPath = connection.parserMemberPath(uri.path);
+        const memberPath = parsedPath.asp ?
+            path.posix.join(parsedPath.asp, parsedPath.library, parsedPath.file, TESTING_CONFIG_FILE) :
+            path.posix.join(parsedPath.library, parsedPath.file, TESTING_CONFIG_FILE);
+        const remoteConfigUri = Uri.from({ scheme: 'member', path: `/${memberPath}` });
+
+        const remoteConfig = await readTestingConfig(remoteConfigUri, 'remote');
+        if (remoteConfig) {
+            Logger.log(LogLevel.Info, `Found remote testing configuration at ${remoteConfigUri.toString()}:\n${JSON.stringify(remoteConfig, null, 2)}`);
+        }
+
+        return remoteConfig;
     }
 
     async function findTestingConfig(workspaceFolder: WorkspaceFolder, uri: Uri): Promise<Uri | undefined> {
@@ -53,28 +68,32 @@ export namespace ConfigHandler {
         }
     };
 
-    async function readLocalTestingConfig(testingConfigUri: Uri): Promise<TestingConfig | undefined> {
+    async function readTestingConfig(testingConfigUri: Uri, type: 'local' | 'remote' | 'global'): Promise<TestingConfig | undefined> {
         try {
             // Check if file exists
             await workspace.fs.stat(testingConfigUri);
         } catch (error: any) {
-            return;
+            Logger.log(LogLevel.Info, `No ${type} testing configuration found at ${testingConfigUri.toString()}`);
         }
 
         try {
             // Read and parse file
-            const content = await workspace.fs.readFile(testingConfigUri);
-            return JSON.parse(content.toString()) as TestingConfig;
+            let testingConfig;
+            if (type === 'local' || type === 'global') {
+                testingConfig = await workspace.fs.readFile(testingConfigUri);
+            } else {
+                const ibmi = getInstance();
+                const connection = ibmi!.getConnection();
+                const content = connection.getContent();
+
+                const parsedPath = connection.parserMemberPath(testingConfigUri.path);
+                testingConfig = await content.downloadMemberContent(parsedPath.library, parsedPath.file, parsedPath.name);
+            }
+
+            return JSON.parse(testingConfig.toString()) as TestingConfig;
         } catch (error: any) {
             Logger.logWithNotification(LogLevel.Error, `Failed to read testing configuration`, `${testingConfigUri} - ${error}`);
             return;
         }
-    }
-
-    export async function getRemoteConfig(uri: Uri): Promise<TestingConfig | undefined> {
-        // TODO: Implement this
-        // LIB/VSCODE/TESTING.JSON file to configure RUCRT* command parameters, relative to member library.
-        // Maybe we have TESTING.JSON member per source file
-        return;
     }
 }
