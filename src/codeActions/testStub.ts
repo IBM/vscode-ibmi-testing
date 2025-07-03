1706d8fca037ca94ddf43785e50272c1d938edde
Sanjula Ganepola <Sanjula.Ganepola@ibm.com>
2025-07-03 13:00:51 -0400

    Add inz keyword for data structures
    Signed-off-by: Sanjula Ganepola <Sanjula.Ganepola@ibm.com>

--- src/codeActions/testStub.ts
+++ src/codeActions/testStub.ts
@@ -459,7 +459,7 @@ export namespace TestStubCodeActions {
             if (detail.type) {
                 declarations.push(`dcl-s ${name} ${detail.type.name}${detail.type.value ? `(${detail.type.value})` : ``};`);
             } else if (detail.reference) {
-                declarations.push(`dcl-ds ${name} likeDs(${detail.reference.name});`);
+                declarations.push(`dcl-ds ${name} likeDs(${detail.reference.name}) inz;`);
             }
         }
 
