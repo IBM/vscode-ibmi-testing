740c3983e9c7981c1a60c17f15055607221f852e
Sanjula Ganepola <32170854+SanjulaGanepola@users.noreply.github.com>
2026-08-27 15:58:33 -0400

    Improve save file installation options (#246)
    * Add support for proxyCmd
    
    Signed-off-by: Sanjula Ganepola <Sanjula.Ganepola@ibm.com>
    
    * Support custom parameters on CODECOV command
    
    Signed-off-by: Sanjula Ganepola <Sanjula.Ganepola@ibm.com>
    
    * Add support for optional github install
    
    Signed-off-by: Sanjula Ganepola <Sanjula.Ganepola@ibm.com>
    
    * Add draft and latest tags
    
    Signed-off-by: Sanjula Ganepola <Sanjula.Ganepola@ibm.com>
    
    ---------
    
    Signed-off-by: Sanjula Ganepola <Sanjula.Ganepola@ibm.com>

---
+++ src/components/rpgUnit/github.ts
@@ -0,0 +1,88 @@
+import { Octokit } from "octokit";
+import * as fs from 'fs';
+import { components } from "@octokit/openapi-types";
+import fetch from "node-fetch";
+import * as path from "path";
+import { OWNER, REPO } from "./version";
+
+export interface Response<T> {
+    data: T,
+    error?: string
+}
+export type Release = components["schemas"]["release"];
+export type ReleaseAsset = components["schemas"]["release-asset"];
+
+export namespace GitHub {
+    export async function getReleases(): Promise<Response<Release[]>> {
+        const releases: Response<Release[]> = {
+            data: []
+        };
+
+        try {
+            const octokit = new Octokit();
+            const response = await octokit.rest.repos.listReleases({
+                owner: OWNER,
+                repo: REPO
+            });
+
+            if (response.status === 200) {
+                releases.data = response.data;
+            } else {
+                releases.error = response.status;
+            }
+        } catch (error: any) {
+            releases.error = error.message ? error.message : error;
+        }
+
+        return releases;
+    }
+
+    export async function getLatestRelease(): Promise<Response<Release>> {
+        const result: Response<Release> = {
+            data: undefined!
+        };
+
+        try {
+            const octokit = new Octokit();
+            const response = await octokit.rest.repos.getLatestRelease({
+                owner: OWNER,
+                repo: REPO
+            });
+
+            if (response.status === 200) {
+                result.data = response.data;
+            } else {
+                result.error = response.status;
+            }
+        } catch (error: any) {
+            result.error = error.message ? error.message : error;
+        }
+
+        return result;
+    }
+
+    export async function downloadReleaseAsset(asset: ReleaseAsset, downloadDirectory: string): Promise<Response<boolean>> {
+        const isDownloaded: Response<boolean> = {
+            data: false
+        };
+
+        try {
+            // Fetch asset
+            const response = await fetch(asset.browser_download_url);
+            const buffer = await response.arrayBuffer();
+
+            // Download asset to specified path
+            if (response.status === 200) {
+                const filePath = path.join(downloadDirectory, asset.name);
+                await fs.promises.writeFile(filePath, Buffer.from(buffer));
+                isDownloaded.data = true;
+            } else {
+                isDownloaded.error = response.statusText;
+            }
+        } catch (error: any) {
+            isDownloaded.error = error.message ? error.message : error;
+        }
+
+        return isDownloaded;
+    }
+}
